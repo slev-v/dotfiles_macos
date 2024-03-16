7bea2477749999b1d6083f91c6f51725ccad4d88
slev-v <stasrudometkin2@gmail.com>
2024-03-16 15:44:16 +0300

    everforest theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,10 +2,39 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-macchiato",
+      colorscheme = "everforest",
     },
   },
 
+  {
+    "sainnhe/everforest",
+    opts = function()
+      local lualine = require("lualine")
+      local custom_everforest = require("lualine.themes.everforest")
+
+      custom_everforest.normal.c.fg = "#293136"
+      custom_everforest.inactive.c.fg = "#293136"
+      custom_everforest.command.c.fg = "#293136"
+      custom_everforest.replace.c.fg = "#293136"
+      custom_everforest.terminal.c.fg = "#293136"
+      custom_everforest.insert.c.fg = "#293136"
+      custom_everforest.visual.c.fg = "#293136"
+
+      custom_everforest.normal.a.fg = "#d8caac"
+      custom_everforest.visual.a.fg = "#d8caac"
+      custom_everforest.insert.a.fg = "#d8caac"
+      custom_everforest.command.a.fg = "#d8caac"
+      custom_everforest.replace.a.fg = "#d8caac"
+      custom_everforest.terminal.a.fg = "#d8caac"
+      custom_everforest.inactive.a.fg = "#d8caac"
+
+      lualine.setup({
+        options = {
+          theme = custom_everforest,
+        },
+      })
+    end,
+  },
   {
     "catppuccin/nvim",
     name = "catppuccin",
